c002c233a019edda3fc5ba6c0e28c82a7facc35e
Ghassenhedfii <3681@holbertonschool.com>
2022-07-11 00:05:37 +0200

    hbtn status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response
-"""
+script that fetches https://intranet.hbtn.io/status"""
 import urllib.request as u
 
 
