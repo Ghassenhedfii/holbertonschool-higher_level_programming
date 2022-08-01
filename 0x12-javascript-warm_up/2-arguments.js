47e95ef99b2e51dad632ee6a5990c4bfb7074f76
Ghassenhedfii <3681@holbertonschool.com>
2022-08-01 15:15:49 +0200

    arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,10 +3,8 @@ const args = process.argv;
 const a = args.length;
 if (a === 2) {
   console.log('No argument');
-}
-else if (a === 3) {
+} else if (a === 3) {
   console.log('Argument found');
-}
-else {
+} else {
   console.log('Arguments found');
 }
