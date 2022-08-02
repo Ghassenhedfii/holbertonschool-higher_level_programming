17b7047318ec0ee451be6fa8f3b6257dd98a22a8
Ghassenhedfii <3681@holbertonschool.com>
2022-08-02 16:45:25 +0200

    square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
-const Square1 = require('./4-rectangle');
-
-module.exports = class Square extends Square1 {
+module.exports = class Square extends require('./5-square') {
     charPrint(c) {
         if (c === undefined) {
-            super.print();
-        } else {
-            for (let i = 0; i < this.width; i++) {
-                console.log(c.repeat(this.height));
+            c = 'X';
+        }
+            for (let i = 0; i < this.height; i++) {
+                console.log(c.repeat(this.width));
             }
         }
     }
-};
+
